db1eba234624c78c6f8c67413d0e2e1a6c3d46e9
Luquinhas <lucmsilva651@gmail.com>
2025-02-08 00:19:28 -0300

    Fixed /mlp

--- src/commands/ponyapi.js
+++ src/commands/ponyapi.js
@@ -12,7 +12,7 @@ module.exports = (bot) => {
   bot.command("mlp", spamwatchMiddleware, async (ctx) => {
     const Strings = getStrings(ctx.from.language_code);
 
-    ctx.reply(Strings.myLittlePonyDesc, {
+    ctx.reply(Strings.ponyApi.helpDesc, {
       parse_mode: 'Markdown',
       disable_web_page_preview: true,
       reply_to_message_id: ctx.message.message_id
