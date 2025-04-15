7120644945a3ad4b5f89f017b3d09d7bf3ce536a
Giovani Finazzi <53719063+GiovaniFZ@users.noreply.github.com>
2025-04-14 21:59:11 -0300

    fix: yt: set cookies for check_size args (#35)
    Co-authored-by: Aidan <aidan@p0ntus.com>

--- src/commands/youtube.js
+++ src/commands/youtube.js
@@ -41,7 +41,12 @@ const downloadFromYoutube = async (command, args) => {
 };
 
 const getApproxSize = async (command, videoUrl) => {
-  const args = [videoUrl, '--compat-opt', 'manifest-filesize-approx', '-O', 'filesize_approx'];
+  let args = [];
+  if (fs.existsSync(path.resolve(__dirname, "../props/cookies.txt"))) {
+    args = [videoUrl, '--compat-opt', 'manifest-filesize-approx', '-O', 'filesize_approx', '--cookies', path.resolve(__dirname, "../props/cookies.txt")];
+  } else {
+    args = [videoUrl, '--compat-opt', 'manifest-filesize-approx', '-O', 'filesize_approx'];
+  }
   try {
     const { stdout } = await downloadFromYoutube(command, args);
     const sizeInBytes = parseInt(stdout.trim(), 10);
@@ -113,9 +118,9 @@ module.exports = (bot) => {
           downloadingMessage.message_id,
           null,
           Strings.ytDownload.downloadingVid, {
-            parse_mode: 'Markdown',
-            reply_to_message_id: ctx.message.message_id,
-          },
+          parse_mode: 'Markdown',
+          reply_to_message_id: ctx.message.message_id,
+        },
         );
 
         const dlpArgs = [videoUrl, ...cmdArgs.split(' '), mp4File];
@@ -126,12 +131,12 @@ module.exports = (bot) => {
           downloadingMessage.message_id,
           null,
           Strings.ytDownload.uploadingVid, {
-            parse_mode: 'Markdown',
-            reply_to_message_id: ctx.message.message_id,
-          },
+          parse_mode: 'Markdown',
+          reply_to_message_id: ctx.message.message_id,
+        },
         );
 
-        if(fs.existsSync(tempMp4File)){
+        if (fs.existsSync(tempMp4File)) {
           await downloadFromYoutube(ffmpegPath, ffmpegArgs);
         }
 
@@ -140,12 +145,13 @@ module.exports = (bot) => {
 
           try {
             await ctx.replyWithVideo({
-              source: mp4File }, {
+              source: mp4File
+            }, {
               caption: message,
               parse_mode: 'Markdown',
               reply_to_message_id: ctx.message.message_id,
             });
-            
+
             fs.unlinkSync(mp4File);
           } catch (error) {
             if (error.response.description.includes("Request Entity Too Large")) {
@@ -154,9 +160,9 @@ module.exports = (bot) => {
                 downloadingMessage.message_id,
                 null,
                 Strings.ytDownload.uploadLimit, {
-                  parse_mode: 'Markdown',
-                  reply_to_message_id: ctx.message.message_id,
-                },
+                parse_mode: 'Markdown',
+                reply_to_message_id: ctx.message.message_id,
+              },
               );
             } else {
               const errMsg = Strings.ytDownload.uploadErr.replace("{error}", error)
@@ -165,9 +171,9 @@ module.exports = (bot) => {
                 downloadingMessage.message_id,
                 null,
                 errMsg, {
-                  parse_mode: 'Markdown',
-                  reply_to_message_id: ctx.message.message_id,
-                },
+                parse_mode: 'Markdown',
+                reply_to_message_id: ctx.message.message_id,
+              },
               );
             };
 
@@ -185,22 +191,21 @@ module.exports = (bot) => {
           downloadingMessage.message_id,
           null,
           Strings.ytDownload.libNotFound, {
-            parse_mode: 'Markdown',
-            reply_to_message_id: ctx.message.message_id,
-          },
+          parse_mode: 'Markdown',
+          reply_to_message_id: ctx.message.message_id,
+        },
         );
       }
     } catch (error) {
-      console.error(error);
       const errMsg = Strings.ytDownload.uploadErr.replace("{error}", error)
       await ctx.telegram.editMessageText(
         ctx.chat.id,
         downloadingMessage.message_id,
         null,
         errMsg, {
-          parse_mode: 'Markdown',
-          reply_to_message_id: ctx.message.message_id,
-        },
+        parse_mode: 'Markdown',
+        reply_to_message_id: ctx.message.message_id,
+      },
       );
     }
   });
