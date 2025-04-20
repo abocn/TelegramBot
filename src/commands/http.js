8aaa60e78d50308620e066a49d8b0da64b37a80c
Lucas Gabriel <lucmsilva651@gmail.com>
2025-04-19 23:52:51 -0300

    Revert "ref: create separated verifyUserInput function (#34)" (#42)
    This reverts commit 12ca87f4fe281be20b1eec5c2dcadc1f0fe8fce2.

--- src/commands/http.js
+++ src/commands/http.js
@@ -3,18 +3,19 @@ const { getStrings } = require('../plugins/checklang.js');
 const { isOnSpamWatch } = require('../spamwatch/spamwatch.js');
 const spamwatchMiddleware = require('../spamwatch/Middleware.js')(isOnSpamWatch);
 const axios = require('axios');
-const { verifyInput } = require('../plugins/verifyInput.js');
 
 module.exports = (bot) => {
   bot.command("http", spamwatchMiddleware, async (ctx) => {
     const Strings = getStrings(ctx.from.language_code);
     const userInput = ctx.message.text.split(' ')[1];
     const apiUrl = Resources.httpApi;
-    const { invalidCode } = Strings.httpCodes
 
-    if (verifyInput(ctx, userInput, invalidCode, true)) {
-      return;
-    }
+    if (!userInput || isNaN(userInput)) {
+      return ctx.reply(Strings.httpCodes.invalidCode, {
+        parse_mode: 'Markdown',
+        reply_to_message_id: ctx.message.message_id
+      });
+    };
 
     try {
       const response = await axios.get(apiUrl);
@@ -49,10 +50,12 @@ module.exports = (bot) => {
   bot.command("httpcat", spamwatchMiddleware, async (ctx) => {
     const Strings = getStrings(ctx.from.language_code);
     const userInput = ctx.message.text.split(' ').slice(1).join(' ').replace(/\s+/g, '');
-    const { invalidCode } = Strings.httpCodes
-
-    if (verifyInput(ctx, userInput, invalidCode, true)) {
-      return;
+    
+    if (!userInput || isNaN(userInput)) {
+      return ctx.reply(Strings.httpCodes.invalidCode, {
+        parse_mode: 'Markdown',
+        reply_to_message_id: ctx.message.message_id
+      });
     }
 
     const apiUrl = `${Resources.httpCatApi}${userInput}`;
