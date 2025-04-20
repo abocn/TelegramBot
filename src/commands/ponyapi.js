3882718d617b8f275974c7e337b2b1ea318cf83f
Lucas Gabriel <lucmsilva651@gmail.com>
2025-04-19 23:52:22 -0300

    Revert "ref: create separated verifyUserInput function (#34)"
    This reverts commit 12ca87f4fe281be20b1eec5c2dcadc1f0fe8fce2.

--- src/commands/ponyapi.js
+++ src/commands/ponyapi.js
@@ -3,7 +3,6 @@ const { getStrings } = require('../plugins/checklang.js');
 const { isOnSpamWatch } = require('../spamwatch/spamwatch.js');
 const spamwatchMiddleware = require('../spamwatch/Middleware.js')(isOnSpamWatch);
 const axios = require("axios");
-const { verifyInput } = require('../plugins/verifyInput.js');
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -23,9 +22,12 @@ module.exports = (bot) => {
   bot.command("mlpchar", spamwatchMiddleware, async (ctx) => {
     const Strings = getStrings(ctx.from.language_code);
     const userInput = ctx.message.text.split(' ').slice(1).join(' ').replace(" ", "+");
-    const { noCharName } = Strings.ponyApi
 
-    if (verifyInput(ctx, userInput, noCharName)) {
+    if (!userInput) {
+      ctx.reply(Strings.ponyApi.noCharName, {
+        parse_mode: 'Markdown',
+        reply_to_message_id: ctx.message.message_id
+      });
       return;
     };
 
@@ -97,9 +99,11 @@ module.exports = (bot) => {
     const Strings = getStrings(ctx.from.language_code);
     const userInput = ctx.message.text.split(' ').slice(1).join(' ').replace(" ", "+");
 
-    const { noEpisodeNum } = Strings.ponyApi
-
-    if (verifyInput(ctx, userInput, noEpisodeNum, true)) {
+    if (!userInput) {
+      ctx.reply(Strings.ponyApi.noEpisodeNum, {
+        parse_mode: 'Markdown',
+        reply_to_message_id: ctx.message.message_id
+      });
       return;
     };
 
@@ -165,9 +169,11 @@ module.exports = (bot) => {
     const Strings = getStrings(ctx.from.language_code);
     const userInput = ctx.message.text.split(' ').slice(1).join(' ').replace(" ", "+");
 
-    const { noComicName } = Strings.ponyApi
-
-    if (verifyInput(ctx, userInput, noComicName)) {
+    if (!userInput) {
+      ctx.reply(Strings.ponyApi.noComicName, {
+        parse_mode: 'Markdown',
+        reply_to_message_id: ctx.message.message_id
+      });
       return;
     };
 
